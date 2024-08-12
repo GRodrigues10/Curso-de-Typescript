7f0379c87552c0fab8c4b1e379352c9792fcd53f
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-12 16:05:10 -0300

    aula 4

---
+++ src/aula4/aula04.ts
@@ -0,0 +1,7 @@
+let nome = 'Gabriel';
+let número = 4.5;
+let valorIndefinido;//Valor indefinido/qualquer(any).
+let valordefinido:number;//Tipo definido mesmo sem um valor.
+
+console.log(nome);
+console.log(número);
