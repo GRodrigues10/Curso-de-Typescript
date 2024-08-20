9461ac3200324dd9caba0f9e17a0d996530740a8
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-20 11:16:22 -0300

    aula 30

---
+++ build/aula30/aula30.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const classes_1 = require("../classes/classes");
+const p1 = new classes_1.Pessoa('Gabriel', 1.80);
+console.log(p1);
+const obj1 = new classes_1.Objeto('Mesa');
+console.log(obj1);
---
+++ src/aula30/aula30.ts
@@ -0,0 +1,6 @@
+import {Pessoa, Objeto} from '../classes/classes';
+
+const p1 = new Pessoa('Gabriel', 1.80);
+console.log(p1);
+const obj1 = new Objeto('Mesa');
+console.log(obj1);
