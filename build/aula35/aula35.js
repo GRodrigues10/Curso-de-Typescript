892174acb0253da7daf901b3154c991f8b0de01e
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-24 11:49:09 -0300

    aula 35

---
+++ build/aula35/aula35.js
@@ -0,0 +1,9 @@
+"use strict";
+const obj = {
+    cor1: 'Verde',
+    cor2: 'Amarelo',
+    cor3: 'Azul',
+    cor4: 'Vermelho'
+};
+let { cor1, cor2, cor3, cor4 } = obj;
+console.log(cor1);
---
+++ src/aula35/aula35.ts
@@ -0,0 +1,22 @@
+// Desestruturação:
+
+// let array = [10, 20, 30, 40];
+
+// let [aa, bb, cc, dd]=array;
+
+// console.log(aa);
+// console.log(bb);
+// console.log(cc);
+// console.log(dd);
+
+const obj = {
+    cor1: 'Verde',
+    cor2: 'Amarelo',
+    cor3: 'Azul',
+    cor4: 'Vermelho'
+}
+
+let {cor1, cor2, cor3, cor4} = obj
+
+console.log(cor1)
+
