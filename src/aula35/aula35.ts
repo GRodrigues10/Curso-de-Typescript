72951fb7aa0106bc3bd32bb63910b26e0b2270eb
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-08-25 12:05:05 -0300

    aula 35

--- src/aula35/aula35.ts
+++ src/aula35/aula35.ts
@@ -16,7 +16,27 @@ const obj = {
     cor4: 'Vermelho'
 }
 
-let {cor1, cor2, cor3, cor4} = obj
+let {cor1, cor2, cor3, cor4} = obj;
 
-console.log(cor1)
+let [nu1, nu2=0,...nu3] = [10, 20, 30, 40, 50, 60];
+
+const _cores = () => {
+    return ['Verde', 'Amarelo', 'Azul', 'Vermelho'];
+}
+
+let [c01, c02, c03, c04] = _cores();
+
+let texto = 'Curso de Typescript';
+
+let [...t] = texto.split(' ');
+let [p1, p2, p3] = texto.split(' ');
+
+console.log(nu1, nu2, nu3);
+console.log(c01, c02, c03, c04);
+console.log(t)
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+
 
